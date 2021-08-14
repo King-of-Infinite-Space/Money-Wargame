6603a600ebaff016fb1b7e92b8bb372be1ef18e2
King-of-Infinite-Space <27502578+King-of-Infinite-Space@users.noreply.github.com>
2021-08-13 23:40:10 -0400

    path match file structure

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,13 @@ const server = http.createServer(app);
 const gameServer = new colyseus.Server({server: server});
 const gameRoom = require('./rooms.js');
 const path = require('path')
-const favicon = require('serve-favicon');
 
 gameServer.register('gameRoom', gameRoom);
 server.listen(port);
 
-app.use('/lib/', express.static(path.join(__dirname, 'lib')));
-app.use('/', express.static(__dirname));
-app.use(favicon(path.join(__dirname, 'favicon.png')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', {root: path.join(__dirname, '../frontend')});
 });
 console.log(`Listening on ws://localhost:${ port }`);
